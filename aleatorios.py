a9d38c9b69636b205a639209022b5657daca4111
Andres Chaparro <you@example.com>
2024-02-07 07:34:41 -0500

    nose

--- aleatorios.py
+++ aleatorios.py
@@ -2,8 +2,8 @@ from random import randint
 n1 = randint (1 , 10)
 n2 = randint (1 , 10)
 if n1 > n2:
-    print print (n1 "Es mayor")
-elif n2 > n1;
+    print (n1, "Es mayor")
+elif n2 > n1:
     print (str(n2) + "Es mayor")
 else:
     print (str(n1) + "=" + str(n2))
